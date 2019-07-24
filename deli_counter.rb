72fc008b06350350c9550fb822ce97996196a40b
Ariela Cohen <arielaades@gmail.com>
2019-07-24 13:28:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ def line(name_array)
     puts "The line is currently empty."
   else
     name_array.each_with_index do |(name, index)|
-      place_in_line = (index + 1)
-      line_array << "#{place_in_line}. #{name}"
+      
+      line_array << "#{index}. #{name}"
     end
     puts "The line is currently: #{line_array}"
   end
