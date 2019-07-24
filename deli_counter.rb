b7379af0eedf844f43a50d62a718f77648825ed6
Ariela Cohen <arielaades@gmail.com>
2019-07-24 13:34:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(name_array)
   if name_array.empty?
     puts "The line is currently empty."
   else
-    name_array.each_with_index(1) do |(name, index)|
+    name_array.each_with_index(1) do |name, index|
 
       line_array << "#{index}. #{name}"
     end
