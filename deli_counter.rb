792ba13cca1ffd26aff9ddbe8d75b2829dd650e1
Ariela Cohen <arielaades@gmail.com>
2019-07-24 13:21:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def line(name_array)
+if name_array.empty
+  puts "The line is currently empty."
+else 
+  name_array.each_with_index do |(name, index)|
+    puts "Welcome, #{name}. You are number #{index+1} in line."
+  end
+end
